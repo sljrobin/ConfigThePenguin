d38cb4cdc35806a024a7da06a1938473a4007a65
sljrobin <sljrobin@gmail.com>
2015-11-21 12:11:38 +0000

    Updated Firefox settings

--- scripts/lib/multi/m_settings_firefox.sh
+++ scripts/lib/multi/m_settings_firefox.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 ########################################################################################################################
-# Show the details of an add-on
+# Multi | Settings | Firefox: show details of an add-on
 #   $1: add-on's name
 #   $2: add-on's URL
-function firefox_addon_show_details()
+function __m_settings_firefox_addonsdetails()
 {
-  colorize "$1" $CLR_LBLUE "n"
+  colorize "> $1" $CLR_DPURPLE "n"
   echo -n " => "
-  colorize "$2" $CLR_LPURPLE "y"
+  colorize "$2" $CLR_DCYAN "y"
 }
 
 
 ########################################################################################################################
-# Show all the URLs of all the add-ons
-function firefox_addon_show_all()
+# Multi | Settings | Firefox: show all add-ons details
+function m_settings_firefox()
 {
-  firefox_addon_show_details "$FFA_BLANKMONITORREADING_NME" "$FFA_BLANKMONITORREADING_URL"
-  firefox_addon_show_details "$FFA_DISCONNECT_NME" "$FFA_DISCONNECT_URL"
-  firefox_addon_show_details "$FFA_FLAGFOX_NME" "$FFA_FLAGFOX_URL"
-  firefox_addon_show_details "$FFA_FTDEEPDARK_NME" "$FFA_FTDEEPDARK_URL"
-  firefox_addon_show_details "$FFA_GOOGLE_SSL_SEARCH_NME" "$FFA_GOOGLE_SSL_SEARCH_URL"
-  firefox_addon_show_details "$FFA_HTTPSEVERYWHERE_NME" "$FFA_HTTPSEVERYWHERE_URL"
-  firefox_addon_show_details "$FFA_NOSCRIPT_NME" "$FFA_NOSCRIPT_URL"
-  firefox_addon_show_details "$FFA_SELFDESTRUCTINGCOOKIES_NME" "$FFA_SELFDESTRUCTINGCOOKIES_URL"
-  firefox_addon_show_details "$FFA_STATUS4EVAR_NME" "$FFA_STATUS4EVAR_URL"
-  firefox_addon_show_details "$FFA_TABMIXPLUS_NME" "$FFA_TABMIXPLUS_URL"
-  firefox_addon_show_details "$FFA_UBLOCKORIGIN_NME" "$FFA_UBLOCKORIGIN_URL"
-  firefox_addon_show_details "$FFA_WORDREFERENCETRANSLATOR_NME" "$FFA_WORDREFERENCETRANSLATOR_URL"
+  colorize "[1/${STP_M_SET_FIREFOX}] Showing all addons details..." $CLR_LBLUE "y"
+  __m_settings_firefox_addonsdetails "$FFA_BLANKMONITORREADING_NME" "$FFA_BLANKMONITORREADING_URL"
+  __m_settings_firefox_addonsdetails "$FFA_DISCONNECT_NME" "$FFA_DISCONNECT_URL"
+  __m_settings_firefox_addonsdetails "$FFA_FLAGFOX_NME" "$FFA_FLAGFOX_URL"
+  __m_settings_firefox_addonsdetails "$FFA_FTDEEPDARK_NME" "$FFA_FTDEEPDARK_URL"
+  __m_settings_firefox_addonsdetails "$FFA_GOOGLE_SSL_SEARCH_NME" "$FFA_GOOGLE_SSL_SEARCH_URL"
+  __m_settings_firefox_addonsdetails "$FFA_HTTPSEVERYWHERE_NME" "$FFA_HTTPSEVERYWHERE_URL"
+  __m_settings_firefox_addonsdetails "$FFA_NOSCRIPT_NME" "$FFA_NOSCRIPT_URL"
+  __m_settings_firefox_addonsdetails "$FFA_SELFDESTRUCTINGCOOKIES_NME" "$FFA_SELFDESTRUCTINGCOOKIES_URL"
+  __m_settings_firefox_addonsdetails "$FFA_STATUS4EVAR_NME" "$FFA_STATUS4EVAR_URL"
+  __m_settings_firefox_addonsdetails "$FFA_TABMIXPLUS_NME" "$FFA_TABMIXPLUS_URL"
+  __m_settings_firefox_addonsdetails "$FFA_UBLOCKORIGIN_NME" "$FFA_UBLOCKORIGIN_URL"
+  __m_settings_firefox_addonsdetails "$FFA_WORDREFERENCETRANSLATOR_NME" "$FFA_WORDREFERENCETRANSLATOR_URL"
+  colorize "[1/${STP_M_SET_FIREFOX}] Showing all addons details..." $CLR_LBLUE "n"
+  colorize " [DONE]" $CLR_LGREEN "y"
 }
