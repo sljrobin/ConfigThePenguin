11fdb466932e63e8dbf48a4240671efdd799f912
sljrobin <sljrobin@gmail.com>
2015-11-23 10:11:59 +0000

    Updated zsh

--- scripts/lib/multi/m_settings_zsh.sh
+++ scripts/lib/multi/m_settings_zsh.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 ########################################################################################################################
-# Check the current shell and if not already set, change to Zsh
-function debian_update_shell()
+# Multi | Settings | Zsh: Check the current shell and if not already set, change to Zsh
+function m_settings_zsh()
 {
-  colorize "Changing the current Shell to Zsh..." $CLR_LPURPLE "y"
-  colorize "Updating Shell [1/${STP_D_CTN_SHELL}] Checking for the current Shell: '$SHELL'" $CLR_LBLUE "y"
+  colorize "[1/${STP_M_SET_ZSH}] Changing current Shell to Zsh..." $CLR_LBLUE "y"
+  # Current Shell
   if [ "$SHELL" == "$DIR_SH_ZSH1" ] || [ "$SHELL" == "$DIR_SH_ZSH2" ]; then
-    colorize "The current Shell is already Zsh." $CLR_LRED "y"
-    colorize "Changing the current Shell to Zsh... [DONE]" $CLR_LGREEN "y"
+    colorize "> The current Shell is already Zsh." $CLR_DORANGE "y"
+  # Change if not set
   else
-    colorize "Updating Shell [2/${STP_D_CTN_SHELL}] Changing the current Shell" $CLR_LBLUE "y"
     chsh -s /bin/zsh
-    colorize "A reboot is necessary" $CLR_LORANGE "y"
-    colorize "Changing the current Shell to Zsh... [DONE]" $CLR_LGREEN "y"
+    colorize "> A reboot is necessary" $CLR_DORANGE "y"
   fi
+  colorize "[1/${STP_M_SET_ZSH}] Changing current Shell to Zsh..." $CLR_LBLUE "n"
+  colorize " [DONE]" $CLR_LGREEN "y"
 }
