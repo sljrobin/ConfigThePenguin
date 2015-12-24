84b894c5fb41acf176125d724bda34442cad05e0
sljrobin <sljrobin@gmail.com>
2015-12-24 13:43:44 +0000

    Updated functions for packages

--- scripts/core/ctp-f_pkgs.sh
+++ scripts/core/ctp-f_pkgs.sh
@@ -4,7 +4,7 @@
 #     Description: 
 #          Author: Simon L. J. Robin | https://sljrobin.org
 #         Created: 2015-12-05 15:03:13
-#        Modified: 2015-12-05 18:59:22
+#        Modified: 2015-12-08 15:57:37
 #
 ########################################################################################################################
 # Packages | Debian | Manager
@@ -29,15 +29,22 @@ function ctp-pkgs_d-install()
   ctp-colorizer "[1/$2] Installing Debian $1 Packages..." $CLR_LBLUE "y"
   # Creating and assigning list of packages
   dpl=$($3)
-  # Installing list of packages
-  __ctp-pkgs_d-manager $dpl
-  # Checking installation status
-  inst_status=$?
-  ctp-colorizer "[1/$2] Installing Debian $1 Packages..." $CLR_LBLUE "n"
-  # Printing installation status
-  if [ $inst_status = 0 ]; then
-    ctp-colorizer " [DONE]" $CLR_LGREEN "y"
+  # Checking list creation status
+  dpl_status=$?
+  # Printing list creation status
+  if [ $dpl_status = 0 ]; then
+    # Installing list of packages
+    __ctp-pkgs_d-manager $dpl
+    # Checking installation status
+    inst_status=$?
+    ctp-colorizer "[1/$2] Installing Debian $1 Packages..." $CLR_LBLUE "n"
+    # Printing installation status
+    if [ $inst_status = 0 ]; then
+      ctp-colorizer " [DONE]" $CLR_LGREEN "y"
+    else
+      ctp-colorizer " [FAIL]" $CLR_LRED "y"
+    fi
   else
-    ctp-colorizer " [FAIL]" $CLR_LRED "y"
+    __ctp-pkgs_d-abort
   fi
 }
