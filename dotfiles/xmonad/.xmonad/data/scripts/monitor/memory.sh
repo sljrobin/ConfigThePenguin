3cb2db1d112ef30c30748abd0a6554be0f589adb
sljrobin <sljrobin@gmail.com>
2016-01-03 17:38:30 +0000

    Modified xmonad files

--- dotfiles/xmonad/.xmonad/data/scripts/monitor/memory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CURRENT=$(free | grep -i mem | awk '{print $3}')
-MAXIMAL=$(free | grep -i mem | awk '{print $2}')
-
-declare -i PERCENT
-PERCENT=($CURRENT*100)/$MAXIMAL
-
-echo "$PERCENT"%
