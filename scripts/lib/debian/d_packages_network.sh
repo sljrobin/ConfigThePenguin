87856f2dd311178f1d425fa442d7c44c5b13680c
sljrobin <sljrobin@gmail.com>
2016-01-30 20:59:15 +0000

    Updated Debian network packages

--- scripts/lib/debian/d_packages_network.sh
+++ scripts/lib/debian/d_packages_network.sh
@@ -2,35 +2,45 @@
 
 ########################################################################################################################
 # Debian | Packages | List Packages for Network
+#    bitlbee: IRC Gateway
 #    curl: tool to transfer a url
 #    ethtool: utility to get information about Network Interface Controllers
 #    geoip-bin: tool to look up the geographic location of an IP address
 #    iceweasel: web browser
+#    fail2ban: IPS
 #    flashplugin-nonfree: Adobe Flash
 #    nginx: HTTP server and reverse proxy
 #    nslookup: tool to manage DNS queries
 #    openvpn: VPN
+#    rkhunter: Rootkit Hunter
 #    rtorrent: BitTorrent client
 #    ssh: Secure Shell
+#    weechat: IRC Client
 #    wicd-curses: wired and wireless network manager
 #    whois: tool for querying Whois database
+#    znc: IRC Bouncer
 function __d_packages-net_list()
 {
   # List
   plnet=$(whiptail --title "Debian | Packages | Network" --checklist \
     "\nSelect Debian Packages to install for Network" 18 97 10 \
+    "bitlbee" "IRC Gateway" OFF \
     "curl" "Tool to transfer a url" ON \
     "ethtool" "Utility to get information about Network Interface Controllers" ON \
     "geoip-bin" "Tool to look up the geographic location of an IP address" ON \
     "iceweasel" "Web browser" ON \
+    "fail2ban" "IPS" OFF \
     "flashplugin-nonfree" "Adobe Flash" ON \
     "nginx" "HTTP server and reverse proxy" OFF \
     "nslookup" "Tool to manage DNS queries" ON \
     "openvpn" "VPN" ON \
+    "rkhunter" "Rootkit Hunter" OFF \
     "rtorrent" "BitTorrent client" ON \
     "ssh" "Secure Shell" ON \
+    "weechat" "IRC Client" ON \
     "wicd-curses" "Wired and wireless network manager" ON \
-    "whois" "Tool for querying Whois database" ON 3>&1 1>&2 2>&3)
+    "whois" "Tool for querying Whois database" ON \
+    "znc" "IRC Bouncer" OFF 3>&1 1>&2 2>&3)
   # Exit status
   pl_status=$?
   # 'OK' option selected
