5b12ab2ea838d7fd9a6b93ec544b66f31f81090c
sljrobin <sljrobin@gmail.com>
2016-03-14 13:26:42 +0000

    Updated icons

--- dotfiles/xmonad/.xmonad/data/scripts/lib/xmd-l_v-icns.sh
+++ dotfiles/xmonad/.xmonad/data/scripts/lib/xmd-l_v-icns.sh
@@ -4,7 +4,7 @@
 #     Description: Variables to store icons
 #          Author: Simon L. J. Robin | https://sljrobin.org
 #         Created: 2015-12-22 17:32:47
-#        Modified: 2016-02-07 01:49:07
+#        Modified: 2016-03-12 00:18:00
 #
 ########################################################################################################################
 ICON_AC="ac_01"
@@ -24,6 +24,7 @@ ICON_MEMORY="mem"
 ICON_MUSIC_PAUSE="pause"
 ICON_MUSIC_PLAY="play"
 ICON_MUSIC_STOP="stop"
+ICON_SD="usb"
 ICON_USB="usb_02"
 ICON_VOLLVL_HIGH="spkr_03"
 ICON_VOLLVL_MEDIUM="spkr_01"
