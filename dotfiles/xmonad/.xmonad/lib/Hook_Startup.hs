ea71978d3172e92d91c9be547b8e2233ba4b1959
sljrobin <sljrobin@gmail.com>
2016-03-26 00:43:27 +0000

    Added LG3D name

--- dotfiles/xmonad/.xmonad/lib/Hook_Startup.hs
+++ dotfiles/xmonad/.xmonad/lib/Hook_Startup.hs
@@ -1,8 +1,10 @@
 module Hook_Startup where
 
 -- Util
+import XMonad.Hooks.SetWMName           -- Sets the WM name
 import XMonad.Util.SpawnOnce            -- Add 'spawnOnce'
 
 myStartupHook = do
+    setWMName "LG3D"                             -- Detect `_NET_SUPPORTING_WM_CHECK` protocol
     spawnOnce "xsetroot -cursor_name left_ptr &" -- Cursor
     spawnOnce "xrdb -merge ~/.Xresources &"      -- Merge xrdb
