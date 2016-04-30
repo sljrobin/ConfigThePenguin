f8b5dd20b56815c85dde63cfeadb31d2ac877b66
sljrobin <sljrobin@gmail.com>
2016-04-30 13:06:04 +0100

    Updated xmonad binding mouse

--- dotfiles/xmonad/.xmonad/lib/Bindings_Mouse.hs
+++ dotfiles/xmonad/.xmonad/lib/Bindings_Mouse.hs
@@ -1,23 +1,41 @@
+-- xmonad: Personal module: Binding Mouse
+-- Author: Simon L. J. Robin | https://sljrobin.org
+--------------------------------------------------------------------------------
 module Bindings_Mouse where
 
--- Core
-import XMonad                           -- Main lib
-import qualified Data.Map        as M   -- Mouse bindings
-import qualified XMonad.StackSet as W   -- ManageHook rules
+--------------------------------------------------------------------------------
+-- * `Data.Map`        -> Mouse bindings
+-- * `XMonad.StackSet` -> ManageHook rules
+-- * `XMonad`          -> Main library
+--------------------------------------------------------------------------------
+import XMonad
+import qualified Data.Map        as M
+import qualified XMonad.StackSet as W
 
 
+--------------------------------------------------------------------------------
+-- Enable/Disable Focus with mouse
+--------------------------------------------------------------------------------
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
+
+
+--------------------------------------------------------------------------------
+-- Binding mouse
+--------------------------------------------------------------------------------
+-- * `M`       -> <Mod>
+-- * `button1` -> Left click
+-- * `button2` -> Middle click
+-- * `button3` -> Right click
+--------------------------------------------------------------------------------
+-- * `M-button1` -> Enables floating mode and drags the current window
+-- * `M-button2` -> Raises the window to the top of the stack
+-- * `M-button3` -> Enables floating mode and resizes by dragging
+--------------------------------------------------------------------------------
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
-    [
-    -- <Mod> + <MouseButton1>: enable floating mode and drag the window
-      ((modMask, button1), (\w -> focus w >> mouseMoveWindow w
+    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w
                                           >> windows W.shiftMaster  ))
-
-    -- <Mod> + <MouseButton2>: raise the window to the top of the stack
     , ((modMask, button2), windows . (W.swapMaster .) . W.focusWindow)
-
-    -- <Mod> + <MouseButton3>: enable floating mode and resize by dragging
     , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w
                                           >> windows W.shiftMaster  ))
     ]
