8cda3bf89cdd5c8bae734d7ed505fcfe493222ab
sljrobin <sljrobin@gmail.com>
2016-05-09 22:50:56 +0100

    Updated xmonad elements

--- dotfiles/xmonad/.xmonad/lib/Elements.hs
+++ dotfiles/xmonad/.xmonad/lib/Elements.hs
@@ -1,45 +1,50 @@
+-- xmonad: Personal module: Elements
+-- Author: Simon L. J. Robin | https://sljrobin.org
+--------------------------------------------------------------------------------
 module Elements where
 
--- Core
-import XMonad                           -- Main lib
--- Actions
-import XMonad.Actions.GridSelect        -- Enable 2D grid
--- Personal Modules
-import Appearance                       -- Load colors, dimensions, and fonts
+--------------------------------------------------------------------------------
+-- * `Appearance`                -> Loads colors, dimensions and fonts
+-- * `XMonad.Actions.GridSelect` -> Enables 2D Grid
+-- * `XMonad`                    -> Main library
+--------------------------------------------------------------------------------
+import XMonad
+import XMonad.Actions.GridSelect
+import Appearance
 
-import XMonad.Layout.IndependentScreens
 
-------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Grid
-------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Colorizer
 myGridColorizer :: Window -> Bool -> X (String, String)
 myGridColorizer = colorRangeFromClassName
-    (0x58,0x58,0x58)                          -- Lowest inactive background
-    (0x58,0x58,0x58)                          -- Highest inactive background
-    (0x7C,0xAF,0xC2)                          -- Active background
-    (0xF8,0xF8,0xF8)                          -- Inactive foreground
-    (0xF8,0xF8,0xF8)                          -- Active foreground
+    (0x58,0x58,0x58)  -- Lowest inactive background
+    (0x58,0x58,0x58)  -- Highest inactive background
+    (0x7C,0xAF,0xC2)  -- Active background
+    (0xF8,0xF8,0xF8)  -- Inactive foreground
+    (0xF8,0xF8,0xF8)  -- Active foreground
 
 -- Configuration
 myGridConfig :: t -> GSConfig Window
 myGridConfig colorizer = (buildDefaultGSConfig myGridColorizer)
-    { gs_cellheight  = 65                     -- Cell height 
-    , gs_cellwidth   = 120                    -- Cell width
-    , gs_cellpadding = 10                     -- Cell padding
-    , gs_font        = myFntGrid              -- Font
+    { gs_cellheight  = 65         -- Cell height 
+    , gs_cellwidth   = 120        -- Cell width
+    , gs_cellpadding = 10         -- Cell padding
+    , gs_font        = myFntGrid  -- Font
     }
 
 
-------------------------------------------------------------------------------------------------------------------------
--- Terminal
-------------------------------------------------------------------------------------------------------------------------
-myTerminal = "urxvtcd"
+--------------------------------------------------------------------------------
+-- Terminals
+--------------------------------------------------------------------------------
+myDmenu    = "dmenu_run -p '->' -i"  -- dmenu
+myTerminal = "urxvtc"               -- Terminal
+myTmux     = "urxvtc -e 'tmux'"     -- tmux
 
 
-------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Workspaces
-------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 myWorkspaces :: [WorkspaceId]
 myWorkspaces = map show [1 .. 9]
---myWorkspaces = withScreens 2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
