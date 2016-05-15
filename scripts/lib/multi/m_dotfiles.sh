b83821a0800f4cacb2c79015dcc935d467afc841
sljrobin <sljrobin@gmail.com>
2016-05-15 23:41:13 +0100

    Updated script for dotfiles

--- scripts/lib/multi/m_dotfiles.sh
+++ scripts/lib/multi/m_dotfiles.sh
@@ -10,6 +10,14 @@ function __m_dotfiles-conky()
   return $?
 }
 
+# Multi | Dotfiles | Create links for Dzen
+function __m_dotfiles-dzen()
+{
+  __ctp-dots_checkd "$DIR_DZEN2"
+  __ctp-dots_symlnk "$DIR_CTP_DZEN2" "$DIR_DZEN2"
+  return $?
+}
+
 # Multi | Dotfiles | Create links for Git
 function __m_dotfiles-git()
 {
@@ -55,6 +63,16 @@ function __m_dotfiles-tmux()
 {
   __ctp-dots_checkf "$DIR_TMUXCONF"
   __ctp-dots_symlnk "$DIR_CTP_TMUXCONF" "$DIR_TMUXCONF"
+  __ctp-dots_checkd "$DIR_TMUX"
+  __ctp-dots_symlnk "$DIR_CTP_TMUX" "$DIR_TMUX"
+  return $?
+}
+
+# Multi | Dotfiles | Create links for urxvt
+function __m_dotfiles-urxvt()
+{
+  __ctp-dots_checkd "$DIR_URXVT"
+  __ctp-dots_symlnk "$DIR_CTP_URXVT" "$DIR_URXVT"
   return $?
 }
 
@@ -68,11 +86,17 @@ function __m_dotfiles-vim()
   return $?
 }
 
-# Multi | Dotfiles | Create links for xinit
-function __m_dotfiles-xinit()
+# Multi | Dotfiles | Create links for X
+function __m_dotfiles-x()
 {
   __ctp-dots_checkf "$DIR_XINITRC"
   __ctp-dots_symlnk "$DIR_CTP_XINITRC" "$DIR_XINITRC"
+  __ctp-dots_checkd "$DIR_XINITRCD"
+  __ctp-dots_symlnk "$DIR_CTP_XINITRCD" "$DIR_XINITRCD"
+  __ctp-dots_checkf "$DIR_XSERVERRC"
+  __ctp-dots_symlnk "$DIR_CTP_XINITRC" "$DIR_XSERVERRC"
+  __ctp-dots_checkf "$DIR_XSESSION"
+  __ctp-dots_symlnk "$DIR_CTP_XINITRC" "$DIR_XSESSION"
   return $?
 }
 
@@ -89,6 +113,8 @@ function __m_dotfiles-xresources()
 {
   __ctp-dots_checkf "$DIR_XRESOURCES"
   __ctp-dots_symlnk "$DIR_CTP_XRESOURCES" "$DIR_XRESOURCES"
+  __ctp-dots_checkd "$DIR_XRESOURCESD"
+  __ctp-dots_symlnk "$DIR_CTP_XRESOURCESD" "$DIR_XRESOURCESD"
   return $?
 }
 
@@ -107,16 +133,18 @@ function __m_dotfiles-zsh()
 # Multi | Dotfiles | Link
 function m_dotfiles_link()
 {
-  __ctp-dots_step "01" "Linking xinit" __m_dotfiles-xinit
+  __ctp-dots_step "01" "Linking X" __m_dotfiles-x
   __ctp-dots_step "02" "Linking Xresources" __m_dotfiles-xresources
-  __ctp-dots_step "03" "Linking xmonad" __m_dotfiles-xmonad
-  __ctp-dots_step "04" "Linking Zsh" __m_dotfiles-zsh
-  __ctp-dots_step "05" "Linking vim" __m_dotfiles-vim
-  __ctp-dots_step "06" "Linking Git" __m_dotfiles-git
-  __ctp-dots_step "07" "Linking tmux" __m_dotfiles-tmux
-  __ctp-dots_step "08" "Linking Conky" __m_dotfiles-conky
-  __ctp-dots_step "09" "Linking rTorrent" __m_dotfiles-rtorrent
-  __ctp-dots_step "10" "Linking Newsbeuter" __m_dotfiles-newsbeuter
-  __ctp-dots_step "11" "Linking MPD" __m_dotfiles-mpd
-  __ctp-dots_step "12" "Linking ncmpcpp" __m_dotfiles-ncmpcpp
+  __ctp-dots_step "03" "Linking rxvt-unicode" __m_dotfiles-urxvt
+  __ctp-dots_step "04" "Linking xmonad" __m_dotfiles-xmonad
+  __ctp-dots_step "05" "Linking Dzen" __m_dotfiles-dzen
+  __ctp-dots_step "06" "Linking Zsh" __m_dotfiles-zsh
+  __ctp-dots_step "07" "Linking vim" __m_dotfiles-vim
+  __ctp-dots_step "08" "Linking Git" __m_dotfiles-git
+  __ctp-dots_step "09" "Linking tmux" __m_dotfiles-tmux
+  __ctp-dots_step "10" "Linking Conky" __m_dotfiles-conky
+  __ctp-dots_step "11" "Linking rTorrent" __m_dotfiles-rtorrent
+  __ctp-dots_step "12" "Linking Newsbeuter" __m_dotfiles-newsbeuter
+  __ctp-dots_step "13" "Linking MPD" __m_dotfiles-mpd
+  __ctp-dots_step "14" "Linking ncmpcpp" __m_dotfiles-ncmpcpp
 }
